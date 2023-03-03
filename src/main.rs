6006c0245b9860d9d3c671878e12e6850d6ec51e
developer1 <3rubasa@gmail.com>
2023-03-04 00:22:33 +0200

    Add basic implementation for open()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::ffi;
+
 fn main() {
-    println!("Hello from Raspberry pi!")
+    // /dev/i2c-1
+    let path_name = ffi::CString::new("/dev/i2c-1").expect("CString::new failed");
+
+    let fd = unsafe {
+        let fd = open(path_name.as_ptr(), 2);
+        fd
+    };
+
+    println!("The devices has been opened: {fd}");
 }
+
+extern "C" {
+    fn open(pathname: *const ffi::c_char, flags: i32) -> i32;
+}
+
+
