c7e71938fe29bbe278dd9b26821dfc0e7ece073e
developer1 <3rubasa@gmail.com>
2023-03-04 13:02:04 +0200

    Add method open()

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,41 @@
 use std::ffi;
 
+const O_RDWR: i32 = 2;
+
+struct Aht20 {
+    fd: i32,
+}
+
+impl Aht20 {
+    fn open(&mut self, pathname: &str) -> bool {
+        let pathname = ffi::CString::new(pathname).expect("Failed to create a CString");
+
+        let fd = unsafe { open(pathname.as_ptr(), O_RDWR) };
+
+        if fd != -1 {
+            self.fd = fd;
+        } else {
+            return false;
+        }
+
+        true
+    }
+}
+
 fn main() {
     // /dev/i2c-1
-    let path_name = ffi::CString::new("/dev/i2c-1").expect("CString::new failed");
+    let mut drv = Aht20 { fd: -1 };
+    let pathname = "/dev/i2c-1";
 
-    let fd = unsafe {
-        let fd = open(path_name.as_ptr(), 2);
-        fd
-    };
+    let res = drv.open(pathname);
 
-    println!("The devices has been opened: {fd}");
+    if res {
+        println!("The device has been opened, fd = {}", drv.fd);
+    } else {
+        println!("Failed to open the device!");
+    }
 }
 
 extern "C" {
     fn open(pathname: *const ffi::c_char, flags: i32) -> i32;
 }
-
-
